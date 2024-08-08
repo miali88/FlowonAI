93d80f0c826d1b7ddca0a5ca22d7f0faf3a9aa20
miali88 <m.ali@gacm.co.uk>
2024-08-08 23:10:53 +0700

    and again

--- backend/scripts/lint.sh
+++ backend/scripts/lint.sh
@@ -4,7 +4,17 @@ set -e
 set -x
 
 echo "Running mypy..."
-mypy app/api || exit 1
+mypy app/api/items || exit 1
+mypy app/api/login || exit 1
+mypy app/api/retellai || exit 1
+mypy app/api/twilio || exit 1
+mypy app/api/users || exit 1
+mypy app/api/vapi || exit 1
+mypy app/api/utils || exit 1
+mypy app/deps || exit 1
+mypy app/main || exit 1
+
+
 mypy app/core || exit 1
 mypy app/email-templates || exit 1
 mypy app/tests || exit 1
