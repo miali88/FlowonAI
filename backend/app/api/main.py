654d0fa6fe39078d1d6457a5423dc14f7696ddbb
miali88 <m.ali@gacm.co.uk>
2024-09-28 11:50:46 +0300

    picking up

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter
 
 from app.api.routes import items, login, users, utils, \
-    retellai, twilio, vapi, onboarding, pipeline, dashboard, chat
+    retellai, twilio, vapi, onboarding, dashboard, chat
 
 api_router = APIRouter()
 
@@ -13,8 +13,8 @@ api_router.include_router(retellai.router, prefix="/retellai", tags=["retellai"]
 api_router.include_router(twilio.router, prefix="/twilio", tags=["twilio"])
 api_router.include_router(vapi.router, prefix="/vapi", tags=["vapi"])
 api_router.include_router(onboarding.router, prefix="/new", tags=["new"])
-api_router.include_router(pipeline.router, prefix="/pipeline", tags=["pipeline"])
 api_router.include_router(dashboard.router, prefix="/dashboard", tags=["dashboard"])
 api_router.include_router(chat.router, prefix="/chat", tags=["chat"])
 
+#api_router.include_router(pipeline.router, prefix="/pipeline", tags=["pipeline"])
 #api_router.include_router(blandai.router, prefix="/blandai", tags=["blandai"])
