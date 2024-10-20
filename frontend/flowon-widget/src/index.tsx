f914fc8c2ddf3e6e13a42c2bcf731d87e9d04027
miali88 <m.ali@gacm.co.uk>
2024-10-20 16:21:22 +0100

    widget built, npm start, uncaught typeerror

--- frontend/flowon-widget/src/index.tsx
+++ frontend/flowon-widget/src/index.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 import App from './App';
 // Import the Web Component to ensure it's registered
 import './FlowonChatWidget';
 
-ReactDOM.render(
+const container = document.getElementById('root');
+const root = createRoot(container!);
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  </React.StrictMode>
 );
-
