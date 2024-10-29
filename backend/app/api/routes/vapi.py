f506f128026737c9ee7fde1dc0c9c2f54806cdd1
miali88 <m.ali@gacm.co.uk>
2024-10-29 21:47:43 +0000

    sentry sdk set up

--- backend/app/api/routes/vapi.py
+++ backend/app/api/routes/vapi.py
@@ -7,3 +7,7 @@ async def vapi_status_update(request: Request):
     request = await request.json()
     print("\n\n vapi /wh", request)
     return 
+
+router.get("/sentry-debug")
+async def trigger_error():
+    division_by_zero = 1 / 0
