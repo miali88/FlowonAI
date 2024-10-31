c9e1f0b27646040c46d63f332340951f78daab6b
miali88 <m.ali@gacm.co.uk>
2024-10-31 02:21:41 +0000

    working widget w/out tailwind

--- widget/vite.config.ts
+++ widget/vite.config.ts
@@ -1,6 +1,5 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import tailwindcss from 'tailwindcss';
 import autoprefixer from 'autoprefixer';
 
 export default defineConfig({
@@ -10,7 +9,7 @@ export default defineConfig({
   },
   css: {
     postcss: {
-      plugins: [tailwindcss, autoprefixer],
+      plugins: [autoprefixer],
     },
   },
   define: {
