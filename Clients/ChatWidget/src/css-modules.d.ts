9adef6d6f28c072502709ad1bf44f90750ddddb2
Michael <m.ali@gacm.co.uk>
2024-12-11 18:15:24 +0000

    seperating chatwidget from clients

--- Clients/ChatWidget/src/css-modules.d.ts
+++
@@ -1,9 +0,0 @@
-declare module '*.module.css' {
-    const classes: { [key: string]: string };
-    export default classes;
-  }
-
-interface CSSStyleDeclaration {
-  backdropFilter: string;
-  WebkitBackdropFilter: string;
-}
