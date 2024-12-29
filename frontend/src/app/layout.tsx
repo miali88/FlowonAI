8c85c974fbe7d857577bfc03e7678e06efdc9c18
miali88 <m.ali@gacm.co.uk>
2024-12-29 15:48:07 +0000

    consent banner added

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -33,6 +33,13 @@ export default function RootLayout({
     <html lang="en" suppressHydrationWarning className={`${geistSans.variable} ${geistMono.variable}`}>
       <head>
         <Script
+          id="Cookiebot"
+          src="https://consent.cookiebot.com/uc.js"
+          data-cbid="211f602c-95ff-4293-8103-42d32dd172fb"
+          type="text/javascript"
+          strategy="afterInteractive"
+        />
+        <Script
           src="https://www.googletagmanager.com/gtag/js?id=G-HTNKTL606H"
           strategy="afterInteractive"
         />
